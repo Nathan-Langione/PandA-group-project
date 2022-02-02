eb466d3694b8b388edd7cc6c0b47c6ffe87ec66b
Nathan Langione <nlangione@wayfair.com>
2022-02-01 20:21:24 -0500

    Adding dans components to main

--- foodBlog/client/src/App.js
+++ foodBlog/client/src/App.js
@@ -1,42 +1,23 @@
+import React from "react";
+import { Router } from '@reach/router';
 
-import './App.css';
+// Import for index/landing/home/dashboard page
+import Dashboard from './views/Dashboard';
+
+// Imports for views
+
+// Imports for components
+import RecipeForm from './components/BlogForm';
+import AllRecipes from './components/All_Recipes';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <div class="form-signin">
-          <form >
-            < h1 class="h3 mb-3 fw-normal">Register</h1>
-            <div class="form-floating">
-              <input type="text" class="form-control" id="floatingInput" placeholder="John" />
-              <label for="floatingInput">First Name</label>
-            </div>
-            <div class="form-floating">
-              <input type="text" class="form-control" id="floatingInput" placeholder="Smith"
-              />
-              <label for="floatingInput">Last Name </label>
-            </div>
-            <div class="form-floating">
-              <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com"
-              />
-              <label for="floatingInput">Email address</label>
-            </div>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
-              />
-              <label for="floatingPassword">Password</label>
-            </div>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Confirm password"
-              />
-              <label for="floatingPassword">Confirm password</label>
-            </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-          </form>
-        </div >
-      </div>
-    </div >
+    <div >
+      <Router>
+        <RecipeForm />
+        {/* <AllRecipes /> */}
+      </Router>
+    </div>
   );
 }
 
